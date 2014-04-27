ead75c1e41429bf3cd21f042d79b5e68a8e707d7
Tatiana Kerick <KerickTatiana@gmail.com>
2014-04-27 17:32:15 -0400

    adding js files, started html

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Tatiana Keric | Porfolio</title>
+
+	<!-- css -->
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+
+	<!-- js -->
+	<script src="js/script.js"></script>
+	<script src="js/jquery.stellar.min.js"></script>
+	<script src="js/waypoints.min.js"></script>
+	<script src="js/query.easing.1.3.js"></script>
+
+</head>
+<body>
+
+<div class="centerWrap">
+
+	<ul class="navigation">
+		<li data-slide="1">Slide 1</li>
+		<li data-slide="2">Slide 2</li>
+		<li data-slide="3">Slide 3</li>
+		<li data-slide="4">Slide 4</li>
+	</ul>
+
+	<div class="slide" id="slide 1">
+		<!-- logo here with wed attributes floating about -->
+	</div>
+
+	<div class="slide" id="slide 2">
+		<!-- gallery starts here, probably going to use angular -->
+	</div>
+
+	<div class="slide" id="slide 3">
+		<!-- php script for contact -->
+	</div>
+
+	<div class="slide" id="slide 4">
+		<!-- not too sure I need a fourth -->
+	</div>
+
+</div><!-- wrapper -->
+
+
+</body>
+</html>
---
+++ js/script.js
@@ -0,0 +1,1 @@
+//need to grab from fancy demo
