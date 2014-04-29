4ad21624ffc41c7cd5ca8bf2a2d3dcf794e90945
Tatiana Kerick <KerickTatiana@gmail.com>
2014-04-28 20:39:06 -0400

    adding content, working on css; ;

--- index.php
+++ index.php
@@ -2,17 +2,12 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>Tatiana Keric | Porfolio</title>
+	<title>Tatiana Kerick | Porfolio</title>
 
 	<!-- css -->
 	<link rel="stylesheet" type="text/css" href="css/reset.css">
 	<link rel="stylesheet" type="text/css" href="css/main.css">
 
-	<!-- js -->
-	<script src="js/script.js"></script>
-	<script src="js/jquery.stellar.min.js"></script>
-	<script src="js/waypoints.min.js"></script>
-	<script src="js/query.easing.1.3.js"></script>
 
 </head>
 <body>
@@ -26,24 +21,49 @@
 		<li data-slide="4">Slide 4</li>
 	</ul>
 
-	<div class="slide" id="slide 1">
-		<img id="logo" src="assets/img/logo.png" />
-	</div>
+	<img id="logo" class="logo" src="assets/img/logo.png" />
 
-	<div class="slide" id="slide 2">
+	<div class="slide" id="slide1" data-slide="1" data-stellar-background-ration="0.5">
+		<div class="wrapper">
+
+			<img src="assets/img/langs/html.png" data-stellar-ratio="3" data-stellar-vertical-offset="-55" alt="">
+			<img src="assets/img/langs/css.png" data-stellar-ratio="1" data-stellar-vertical-offset="-102" alt="">
+			<img src="assets/img/langs/javascript.png" data-stellar-ratio="1.5" data-stellar-vertical-offset="-53" alt="">
+			<img src="assets/img/langs/jquery.png" data-stellar-ratio="2.7" data-stellar-vertical-offset="-25" alt="">
+			<img src="assets/img/langs/angular.png" data-stellar-ratio="1" alt="">
+			<img src="assets/img/langs/mysql.png" data-stellar-ratio="2.7" alt="">
+			<img src="assets/img/langs/mongo.png" data-stellar-ratio="3" alt="">
+			<img src="assets/img/langs/php.png" data-stellar-ratio="1" alt="">
+			<img src="assets/img/langs/cake.png" data-stellar-ratio="1.5" alt="">
+			<img src="assets/img/langs/codeigniter.png" data-stellar-ratio="3" alt="">
+		</div><!-- wrapper -->
+
+		<a class="button" data-slide="2" title=""></a>
+
+	</div><!-- slide1 -->
+
+	<div class="slide" id="slide2">
 		<!-- gallery starts here, probably going to use angular -->
-	</div>
+	</div><!-- slide2 -->
 
-	<div class="slide" id="slide 3">
+	<div class="slide" id="slide3">
 		<!-- php script for contact -->
-	</div>
+	</div><!-- slide3 -->
 
-	<div class="slide" id="slide 4">
+	<div class="slide" id="slide4">
 		<!-- not too sure I need a fourth -->
-	</div>
+	</div><!-- slide4 -->
 
-</div><!-- wrapper -->
+</div><!-- centerWrap -->
 
+
+<!-- js -->
+	
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+<script src="js/jquery.stellar.min.js"></script>
+<script src="js/waypoints.min.js"></script>
+<script src="js/query.easing.1.3.js"></script>
+<script src="js/script.js"></script>
 
 </body>
 </html>
