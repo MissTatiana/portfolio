787cb744a0d02c4ee9401602910ec764d87d6c4c
Tatiana Kerick <KerickTatiana@gmail.com>
2014-07-03 02:39:37 -0400

    new index and css, same parallax, different nav thing

--- js/script.js
+++ js/script.js
@@ -31,5 +31,17 @@ slide.waypoint(function (event, direction) {
 
 });//slide.waypoint
 
+
+// STICK NAVIGATION
+$(window).bind('scroll', function() {
+var navHeight = $( window ).height() - 70;
+     if ($(window).scrollTop() > navHeight) {
+         $('nav').addClass('fixed');
+     }
+     else {
+         $('nav').removeClass('fixed');
+     }
 });
+
+});//document
 
