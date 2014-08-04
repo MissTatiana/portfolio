79e2272d618c4645b7324551db8880eb089d9f26
Tatiana Kerick <KerickTatiana@gmail.com>
2014-08-04 01:37:31 -0400

    reg body pages changed, tried to work on the form

--- contact.php
+++ contact.php
@@ -16,94 +16,148 @@
 
 <body>
 
-	<div class="regNav">
+<div class="regNav">
 	<ul>
 		<li><a href="index.html#about">About</a></li>
-		<li><a href="index.html#services">Services</a></li>
 		<li><a href="portfolio.html">Portfolio</a></li>
 		<li><a href="contact.php">Contact</a></li>
 	</ul>
-	</div>
+</div>
 
-	<div class="regBody">
-		<span class="regDepth" title="contact">Contact</span>
+<div class="regBody">
 
-		<div id="icons">
-			<a target="_blank" href="https://github.com/MissTatiana" alt="github"><img src="assets/img/icons/github.png"></a>
-			&nbsp;
-			&nbsp;
-			<a target="_blank" href="https://www.linkedin.com/pub/tatiana-kerick/82/522/4?trk=shareTw" alt="linked in"><img src="assets/img/icons/linked.png"></a>
-			&nbsp;
-			&nbsp;
-			<a target="_blank" href="assets/img/Resume.pdf" alt="resume"><img src="assets/img/icons/resume.png"></a>
-		</div><!-- icons -->
+	<h1>Contact Me</h1>
 
-		<form id="contact">
-			
-			<div class="name">
-				<!-- first name -->
-				<label for="firstName">First Name:</label>
+	<div id="icons">
+		<a target="_blank" href="https://github.com/MissTatiana" alt="github"><img src="assets/img/icons/github.png"></a>
+		&nbsp;
+		&nbsp;
+		<a target="_blank" href="https://www.linkedin.com/pub/tatiana-kerick/82/522/4?trk=shareTw" alt="linked in"><img src="assets/img/icons/linked.png"></a>
+		&nbsp;
+		&nbsp;
+		<a target="_blank" href="assets/img/Resume.pdf" alt="resume"><img src="assets/img/icons/resume.png"></a>
+	</div><!-- icons -->
 
-				<input type="text" name="firstName" id="firstName" required />
-			</div><!-- name -->
+	<form id="contact" method="post">
+		
+		<div class="name">
+			<!-- first name -->
+			<label for="firstName">First Name:</label>
 
-			<div>
-				<!-- last name -->
-				<label for="lastName">Last Name:</label>
+			<input type="text" name="firstName" id="firstName" required />
+		</div><!-- name -->
 
-				<input type="text" name="lastName" id="lastName" required />
-			</div><!-- name -->
+		<div>
+			<!-- last name -->
+			<label for="lastName">Last Name:</label>
 
-			<div>
-				<!-- email -->
-				<label for="email">Email Address:</label>
+			<input type="text" name="lastName" id="lastName" required />
+		</div><!-- name -->
 
-				<input type="email" name="email" id="email" required />
-			</div>
+		<div>
+			<!-- email -->
+			<label for="email">Email Address:</label>
 
-			<div>
-				<!-- subject -->
-				<label for="subject">Subject:</label>
+			<input type="email" name="email" id="email" required />
+		</div>
 
-				<input type="text" name="subject" id="subject" required />
-			</div>
+		<div>
+			<!-- subject -->
+			<label for="subject">Subject:</label>
 
-			<div>
-				<!-- message -->
-				<label for="message">Message:</label>
+			<input type="text" name="subject" id="subject" required />
+		</div>
 
-				<textarea name="message" id="message" rows="10" cols="76"></textarea>
-			</div>
+		<div>
+			<!-- message -->
+			<label for="message">Message:</label>
 
-			<input type="submit" id="sendBtn" class="btn" value="SEND" />
+			<textarea name="message" id="message" rows="10" cols="76"></textarea>
+		</div>
 
-		</form>
+		<input type="submit" id="sendBtn" class="btn" value="SEND" />
 
-		<div class="footer" id="contactFooter">
-			<a href="index.html#about">About</a>
-			&nbsp;
-			|
-			&nbsp;
-			<a href="index.html#services">Services</a>
-			&nbsp;
-			|
-			&nbsp;
-			<a href="portfolio.html">Portfolio</a>
-			&nbsp;
-			|
-			&nbsp;
-			<a href="contact.php">Contact</a>
+	</form>
+</div>
 
-			<br />
-			<br />
+<?php 
+	if(isset($_POST["email"])) {
 
-			<p>Tatiana Kerick 2014&copy;
-			<br />
-			KerickTatiana@gmail.com</p>
-		</div><!-- footer -->
+		$email_to = "KerickTatiana@gmail.com";
 
-	</div><!-- regBody -->
+		$fistName = $_POST["firstName"];
+		$lastName = $_POST["lastName"];
+		$email = $_POST["email"];
+		$subject = $_POST["subject"];
+		$message = $_POST["message"];
 
+		$errorMessage = "";
+
+		$emailMessage = "form details below.\n\n";
+
+		function died($error) {
+			//errors here
+		}
+
+		//validation expected data exists
+		if(!isset($_POST["firstName"]) ||
+				!isset($_POST["lastName"]) ||
+				!isset($_POST["email"]) ||
+				!isset($_POST["subject"]) ||
+				!isset($_POST["message"])) {
+			died("Please complete the form properly");
+		}
+
+		$emailExp = "/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/";
+		$stringExp = "/^[A-Za-z .'-]+$/";
+
+		if(!preg_match($emailExp, $email)) {
+			$errorMessage .= "Invalid email address";
+		}
+
+		if(!preg_match($stringExp, $firstName)) {
+			$errorMessage = "Invalid first name";
+		}
+
+		if(!preg_match($stringExp, $lastName)) {
+			$errorMessage = "Invalid last name";
+		}
+
+		if(!preg_match($stringExp, $subject)) {
+			$errorMessage = "Invalid subject";
+		}
+
+		if(strlen($message) < 2) {
+			$errorMessage = "The message you entered isn't valid";
+		}
+
+		function cleanString($string) {
+			$bad = array("content-type","bcc:","to:","cc:","href");
+
+			return str_replace($bad, "", $string);
+		}
+
+		$emailMessage .= "First Name: " . cleanString($firstName)."\n";
+		$emailMessage .= "Last Name: " . cleanString($lastName);
+		$emailMessage .= "Email: " . cleanString($email);
+		$emailMessage .= "Message: " . cleanString($message);
+
+		echo $emailMessage;
+
+		//create email headers
+		$headers = "Form: " . $firstName . " " . $lastName . "\r\n" . 
+
+		"Reply-To: " . $email . "\r\n" . 
+
+		"X-Mailer: PHP/" . phpversion();
+
+		@mail($email_to, $subject, $emailMessage);
+
+
+
+	}//isset post email
+
+?>
 
 <!-- My script -->
 <script src="js/script.js"></script>
