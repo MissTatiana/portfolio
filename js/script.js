30f236acb55da8a517c6296dc602761e3d3bca43
Tatiana Kerick <KerickTatiana@gmail.com>
2015-04-01 21:37:40 -0400

    modal fuctioning, but needs to be one at a time

--- js/script.js
+++ js/script.js
@@ -1,16 +1,15 @@
 $(function() {
 
-	var counter = 0;
-	$('.sites').click(function() {
-		counter++;
+	//hide modal
+	$('.mask').hide();
 
-		if(counter % 2 === 0 ) {
-			$(this).removeClass('large-12');
-			$(this).addClass('large-4');
-		} else {
-			$(this).removeClass('large-4');
-			$(this).addClass('large-12');
-		}
+	$('.sites').click(function() {
+		$('.mask').show();
+		
 	});
+
+	$('.fi-x').click(function() {
+		$('.mask').hide();
+	})
 
 });
--- js/sites.js
+++ js/sites.js
@@ -8,11 +8,9 @@ var project = {
 			technology: 'the technology used',
 			description: 'Cupcake ipsum dolor sit amet candy fruitcake. Muffin jujubes liquorice macaroon oat cake cupcake croissant bonbon. Dessert dragée danish bonbon gummies cake cake. Dessert danish danish ice cream tart sweet pie. Marshmallow marzipan lemon drops dessert cake cupcake.',
 			thumb: 'assets/img/temp/thumb.jpg',
-			pictures: {
-				url: '/assets/img/temp/placeholder1.jpg',
-				url: '/assets/img/temp/placeholder2.jpg',
-				url: '/assets/img/temp/placeholder3.jpg'
-			}
+			sitePicture: 'assets/img/temp/placeholder1.jpg',
+			git: 'git',
+			url: 'link'
 		},
 		{
 			projectTitle: 'Project 2',
@@ -73,7 +71,8 @@ var project = {
 				url: '/assets/img/temp/placeholder2.jpg',
 				url: '/assets/img/temp/placeholder3.jpg'
 			}
-		}
+		} 
+		
 	]
 };
 
@@ -87,3 +86,66 @@ $('.project-placeholder').html(theCompiledProjects);
 
 
 });
+
+/*
+{
+			projectTitle: 'Project 2',
+			date: 'Month 00',
+			technology: 'the technology used',
+			description: 'Cupcake ipsum dolor sit amet candy fruitcake. Muffin jujubes liquorice macaroon oat cake cupcake croissant bonbon. Dessert dragée danish bonbon gummies cake cake. Dessert danish danish ice cream tart sweet pie. Marshmallow marzipan lemon drops dessert cake cupcake.',
+			thumb: 'assets/img/temp/thumb.jpg',
+			pictures: {
+				url: '/assets/img/temp/placeholder1.jpg',
+				url: '/assets/img/temp/placeholder2.jpg',
+				url: '/assets/img/temp/placeholder3.jpg'
+			}
+		},
+		{
+			projectTitle: 'Project 3',
+			date: 'Month 00',
+			technology: 'the technology used',
+			description: 'Cupcake ipsum dolor sit amet candy fruitcake. Muffin jujubes liquorice macaroon oat cake cupcake croissant bonbon. Dessert dragée danish bonbon gummies cake cake. Dessert danish danish ice cream tart sweet pie. Marshmallow marzipan lemon drops dessert cake cupcake.',
+			thumb: 'assets/img/temp/thumb.jpg',
+			pictures: {
+				url: '/assets/img/temp/placeholder1.jpg',
+				url: '/assets/img/temp/placeholder2.jpg',
+				url: '/assets/img/temp/placeholder3.jpg'
+			}
+		},
+		{
+			projectTitle: 'Project 4',
+			date: 'Month 00',
+			technology: 'the technology used',
+			description: 'Cupcake ipsum dolor sit amet candy fruitcake. Muffin jujubes liquorice macaroon oat cake cupcake croissant bonbon. Dessert dragée danish bonbon gummies cake cake. Dessert danish danish ice cream tart sweet pie. Marshmallow marzipan lemon drops dessert cake cupcake.',
+			thumb: 'assets/img/temp/thumb.jpg',
+			pictures: {
+				url: '/assets/img/temp/placeholder1.jpg',
+				url: '/assets/img/temp/placeholder2.jpg',
+				url: '/assets/img/temp/placeholder3.jpg'
+			}
+		},
+		{
+			projectTitle: 'Project 5',
+			date: 'Month 00',
+			technology: 'the technology used',
+			description: 'Cupcake ipsum dolor sit amet candy fruitcake. Muffin jujubes liquorice macaroon oat cake cupcake croissant bonbon. Dessert dragée danish bonbon gummies cake cake. Dessert danish danish ice cream tart sweet pie. Marshmallow marzipan lemon drops dessert cake cupcake.',
+			thumb: 'assets/img/temp/thumb.jpg',
+			pictures: {
+				url: '/assets/img/temp/placeholder1.jpg',
+				url: '/assets/img/temp/placeholder2.jpg',
+				url: '/assets/img/temp/placeholder3.jpg'
+			}
+		},
+		{
+			projectTitle: 'Project 6',
+			date: 'Month 00',
+			technology: 'the technology used',
+			description: 'Cupcake ipsum dolor sit amet candy fruitcake. Muffin jujubes liquorice macaroon oat cake cupcake croissant bonbon. Dessert dragée danish bonbon gummies cake cake. Dessert danish danish ice cream tart sweet pie. Marshmallow marzipan lemon drops dessert cake cupcake.',
+			thumb: 'assets/img/temp/thumb.jpg',
+			pictures: {
+				url: '/assets/img/temp/placeholder1.jpg',
+				url: '/assets/img/temp/placeholder2.jpg',
+				url: '/assets/img/temp/placeholder3.jpg'
+			}
+		} 
+*/
