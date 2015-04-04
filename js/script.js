9b7b373e9a2b631089ad95dfcfbf744deaa3c05f
Tatiana Kerick <KerickTatiana@gmail.com>
2015-04-04 01:47:03 -0400

    tag imgs added, animation

--- js/script.js
+++ js/script.js
@@ -1,17 +1,69 @@
 $(function() {
 
-	$('#eventH5').mouseover(function() {
-		console.log('testing');
+	
+	/* logo
+	---------------------------*/
+	$('#logo').hide()
 
-		var styles = {
-			'height': '300px',
-			'top': '-300px',
-			'background': 'rgba(0, 0, 0, 0.80)',
-			'color': '#8c7f91'
-		};
+	$('#logo').fadeIn(1000);
 
-		$(this).css( styles );
-		$('#eventSpan').css( 'color', '#a699be')
-	})
+	$('#logo').animate({
+		top: '-=200'
+	}, 500);
+
+
+	/* tag images
+	---------------------------*/
+	var designer = $('#designer');
+
+	designer.hide();
+	designer.delay(1500).fadeIn(10);
+	designer.animate({
+		top: '-=200'
+	});
+	designer.animate({
+		top: '+=80'
+	});
+	designer.animate({
+		top: '-=20'
+	});
+
+
+	var developer = $('#developer')
+
+	developer.hide();
+	developer.hide();
+	developer.delay(1700).fadeIn(10);
+	developer.animate({
+		top: '-=200'
+	});
+	developer.animate({
+		top: '+=80'
+	});
+	developer.animate({
+		top: '-=20'
+	});
+
+
+	var extraoddd = $('#extraodd')
+
+	extraoddd.hide();
+	extraoddd.hide();
+	extraoddd.hide();
+	extraoddd.delay(1900).fadeIn(10);
+	extraoddd.animate({
+		top: '-=200'
+	});
+	extraoddd.animate({
+		top: '+=80'
+	});
+	extraoddd.animate({
+		top: '-=20'
+	});
+
+
+	$('.h3').hide();
+	$('.h3').delay(2600).fadeIn(20);
+
 
 });
