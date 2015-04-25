c4f98b6ba6398f3329fbe364059c259f73396d4c
Tatiana Kerick <KerickTatiana@gmail.com>
2015-04-25 15:12:04 -0400

    about me section started

--- js/script.js
+++ js/script.js
@@ -1,69 +1,69 @@
 $(function() {
 
 	
-	/* logo
-	---------------------------*/
-	$('#logo').hide()
+	// /* logo
+	// ---------------------------*/
+	// $('#logo').hide()
 
-	$('#logo').fadeIn(1000);
+	// $('#logo').fadeIn(1000);
 
-	$('#logo').animate({
-		top: '-=200'
-	}, 500);
-
-
-	/* tag images
-	---------------------------*/
-	var designer = $('#designer');
-
-	designer.hide();
-	designer.delay(1500).fadeIn(10);
-	designer.animate({
-		top: '-=200'
-	});
-	designer.animate({
-		top: '+=80'
-	});
-	designer.animate({
-		top: '-=20'
-	});
+	// $('#logo').animate({
+	// 	top: '-=200'
+	// }, 500);
 
 
-	var developer = $('#developer')
+	// /* tag images
+	// ---------------------------*/
+	// var designer = $('#designer');
 
-	developer.hide();
-	developer.hide();
-	developer.delay(1700).fadeIn(10);
-	developer.animate({
-		top: '-=200'
-	});
-	developer.animate({
-		top: '+=80'
-	});
-	developer.animate({
-		top: '-=20'
-	});
-
-
-	var extraoddd = $('#extraodd')
-
-	extraoddd.hide();
-	extraoddd.hide();
-	extraoddd.hide();
-	extraoddd.delay(1900).fadeIn(10);
-	extraoddd.animate({
-		top: '-=200'
-	});
-	extraoddd.animate({
-		top: '+=80'
-	});
-	extraoddd.animate({
-		top: '-=20'
-	});
+	// designer.hide();
+	// designer.delay(1500).fadeIn(10);
+	// designer.animate({
+	// 	top: '-=200'
+	// });
+	// designer.animate({
+	// 	top: '+=80'
+	// });
+	// designer.animate({
+	// 	top: '-=20'
+	// });
 
 
-	$('.h3').hide();
-	$('.h3').delay(2600).fadeIn(20);
+	// var developer = $('#developer')
+
+	// developer.hide();
+	// developer.hide();
+	// developer.delay(1700).fadeIn(10);
+	// developer.animate({
+	// 	top: '-=200'
+	// });
+	// developer.animate({
+	// 	top: '+=80'
+	// });
+	// developer.animate({
+	// 	top: '-=20'
+	// });
+
+
+	// var extraoddd = $('#extraodd')
+
+	// extraoddd.hide();
+	// extraoddd.hide();
+	// extraoddd.hide();
+	// extraoddd.delay(1900).fadeIn(10);
+	// extraoddd.animate({
+	// 	top: '-=200'
+	// });
+	// extraoddd.animate({
+	// 	top: '+=80'
+	// });
+	// extraoddd.animate({
+	// 	top: '-=20'
+	// });
+
+
+	// $('.h3').hide();
+	// $('.h3').delay(2600).fadeIn(20);
 
 
 
