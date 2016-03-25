796f3c7329df748e0eb105b6e2ece44b19e4f562
Tatiana Kerick <KerickTatiana@gmail.com>
2016-03-25 01:14:33 -0400

    animation complete

--- js/script.js
+++ js/script.js
@@ -1,3 +1,78 @@
 $(window).load( function() {
 	$(".se-pre-con").fadeOut("slow");
 });
+
+// LOGO ANIMATION
+$("#logo").animate({ 
+	top: '-=95',
+	opacity: 1
+}, 500);
+
+$("#logo").animate({ 
+	top: '+=20',
+}, 500);
+
+
+// SUB LOGO ANIMATION
+$("#sublogo").delay(700).animate({ 
+	top: '-=95',
+	opacity: 1
+}, 500);
+
+$("#sublogo").animate({ 
+	top: '+=20',
+}, 500);
+
+
+
+//SKILL ANIMATION
+$("#sass").delay(1300).animate({ 
+	top: '-=95',
+	opacity: 1
+}, 500);
+
+$("#sass").animate({ 
+	top: '+=20',
+}, 500);
+
+
+$("#stack").delay(1500).animate({ 
+	top: '-=95',
+	opacity: 1
+}, 500);
+
+$("#stack").animate({ 
+	top: '+=20',
+}, 500);
+
+
+$("#ui").delay(1700).animate({ 
+	top: '-=95',
+	opacity: 1
+}, 500);
+
+$("#ui").animate({ 
+	top: '+=20',
+}, 500);
+
+
+$("#agile").delay(1900).animate({ 
+	top: '-=95',
+	opacity: 1
+}, 500);
+
+$("#agile").animate({ 
+	top: '+=20',
+}, 500);
+
+
+//ABOUT FADE IN
+$(".about").delay(2100).animate({
+	opacity: 1
+})
+
+
+
+
+
+
